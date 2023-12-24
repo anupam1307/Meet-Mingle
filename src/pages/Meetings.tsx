448b040032e730e412bc2682492e13d499ecf547
Anupam Kumar <123089147+anupam1307@users.noreply.github.com>
2023-12-24 19:43:29 +0530

    vercel deploy

--- src/pages/Meetings.tsx
+++ src/pages/Meetings.tsx
@@ -1,119 +1,135 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from "react-router-dom";
-import moment from "moment"
-import { MeetingType } from '../utils/Types'
-import { getDocs, query, where } from 'firebase/firestore'
-import { meetingRef } from '../utils/FirebaseConfig'
-import { useAppSelector } from '../app/hooks'
-import useAuth from '../hooks/useAuth'
-import Header from '../components/Header'
-import { EuiBadge, EuiBasicTable, EuiButton, EuiButtonIcon, EuiCopy, EuiFlexGroup } from '@elastic/eui'
-import { EuiFlexItem } from '@elastic/eui'
-import { EuiPanel } from '@elastic/eui'
-
-
-export default function Meeting() {
+import {
+    EuiBadge,
+    EuiBasicTable,
+    EuiButtonIcon,
+    EuiCopy,
+    EuiFlexGroup,
+    EuiFlexItem,
+    EuiPanel,
+  } from "@elastic/eui";
+  
+  import { getDocs, query } from "firebase/firestore";
+  import moment from "moment";
+  import React, { useEffect, useState } from "react";
+  import { Link } from "react-router-dom";
+  import { useAppSelector } from "../app/hooks";
+  import Header from "../components/Header";
+  import useAuth from "../hooks/useAuth";
+  import { meetingRef } from "../utils/FirebaseConfig";
+import { MeetingType } from "../utils/Types";
+  
+  export default function Meeting() {
     useAuth();
-    const [meetings, setMeetings] = useState<any>([])
-    const userInfo = useAppSelector((zoom)=>zoom.auth.userInfo)
-
-    useEffect(()=> {
-      if (userInfo){
-        const getUserMeetings = async ()=> {
-            const firestoreQuery = query(meetingRef);
-            const fetchedMeetings = await getDocs(firestoreQuery);
-            if (fetchedMeetings.docs.length) {
-                const myMeetings: Array<MeetingType> = [];
-                fetchedMeetings.forEach(meetings=> {
-                    const data = meetings.data() as MeetingType;
-                    if(data.createdBy===userInfo?.uid) myMeetings.push(data);
-                    else if(data.meetingType==="anyone-can-join")myMeetings.push(data);
-                    else{
-                        const index = data.invitedUsers.findIndex(user=>user===userInfo.uid)
-                        if(index!==-1){
-                            myMeetings.push(data);
-                        }
-                    }
-                });
-                setMeetings(myMeetings);   
-            } 
-        };
-        getUserMeetings();
-      }
-    },[userInfo]);
-
-    const columns =[
-        {
-            field:"meetingName",
-            name:"Meeting Name"
-        },
-        {
-            field:"meetingType",
-            name:"Meeting Type"
-        },
-        {
-            field:"meetingDate",
-            name:"Meeting Date"
-        },
-        {
-            field:"",
-            name:"Status",
-            render:(meeting:MeetingType)=>{
-                if(meeting.status){
-                    if(meeting.meetingDate === moment().format("L")){
-                        return ( <EuiBadge color = "success">
-                            <Link 
-                            style={{color:"black"}}
-                            to={`/Join.${meeting.meetingId}`}>
-                                Join Now
-                            </Link>
-                        </EuiBadge>)
-                    } else if(moment(meeting.meetingDate).isBefore(moment().format('L'))){
-                        return <EuiBadge color='default'>Ended</EuiBadge>
-                    } else {
-                        return <EuiBadge color='primary'>Upcoming</EuiBadge>
-                    }
-                }else return <EuiBadge color="danger">Cancelled</EuiBadge>
+    const userInfo = useAppSelector((zoom) => zoom.auth.userInfo);
+    const [meetings, setMeetings] = useState<Array<MeetingType>>([]);
+  
+    useEffect(() => {
+      const getMyMeetings = async () => {
+        const firestoreQuery = query(meetingRef);
+        const fetchedMeetings = await getDocs(firestoreQuery);
+        if (fetchedMeetings.docs.length) {
+          const myMeetings: Array<MeetingType> = [];
+          fetchedMeetings.forEach((meeting) => {
+            const data = meeting.data() as MeetingType;
+            if (data.createdBy === userInfo?.uid)
+              myMeetings.push(meeting.data() as MeetingType);
+            else if (data.meetingType === "anyone-can-join")
+              myMeetings.push(meeting.data() as MeetingType);
+            else {
+              const index = data.invitedUsers.findIndex(
+                (user: string) => user === userInfo?.uid
+              );
+              if (index !== -1) {
+                myMeetings.push(meeting.data() as MeetingType);
+              }
             }
-
+          });
+  
+          setMeetings(myMeetings);
+        }
+      };
+      if (userInfo) getMyMeetings();
+    }, [userInfo]);
+  
+    const meetingColumns = [
+      {
+        field: "meetingName",
+        name: "Meeting Name",
+      },
+      {
+        field: "meetingType",
+        name: "Meeting Type",
+      },
+      {
+        field: "meetingDate",
+        name: "Meeting Date",
+      },
+      {
+        field: "",
+        name: "Status",
+  
+        render: (meeting: MeetingType) => {
+          if (meeting.status) {
+            if (meeting.meetingDate === moment().format("L")) {
+              return (
+                <EuiBadge color="success">
+                  <Link
+                    to={`/join/${meeting.meetingId}`}
+                    style={{ color: "black" }}
+                  >
+                    Join Now
+                  </Link>
+                </EuiBadge>
+              );
+            } else if (
+              moment(meeting.meetingDate).isBefore(moment().format("L"))
+            ) {
+              return <EuiBadge color="default">Ended</EuiBadge>;
+            } else if (moment(meeting.meetingDate).isAfter()) {
+              return <EuiBadge color="primary">Upcoming</EuiBadge>;
+            }
+          } else return <EuiBadge color="danger">Cancelled</EuiBadge>;
         },
-        {
-           field:"meetingId",
-           name:"Copy Link",
-           render:(meetingId:string) => {
-            return(
-            <EuiCopy textToCopy={`${process.env.REACT_APP_HOST}/join/${meetingId}`}>
-                {(copy)=>(
-                    <EuiButtonIcon 
-                    iconType="copy"
-                    onClick={copy}
-                    display='base'
-                    aria-label='Meeting-copy'
-                    />
-                    )}
+      },
+      {
+        field: "meetingId",
+        name: "Copy Link",
+        width: "10%",
+        render: (meetingId: string) => {
+          return (
+            <EuiCopy
+              textToCopy={`${process.env.REACT_APP_HOST}/join/${meetingId}`}
+            >
+              {(copy: any) => (
+                <EuiButtonIcon
+                  iconType="copy"
+                  onClick={copy}
+                  display="base"
+                  aria-label="meeting-copy"
+                />
+              )}
             </EuiCopy>
-            );
-           },
+          );
         },
+      },
     ];
-  return (
-    <div 
-      style={{
-      display: "flex",
-      height: "100vh",
-      flexDirection: "column",
-      }}
-    >
-    <Header/>
-    <EuiFlexGroup justifyContent ="center"  style={{margin:"1rem"}}>
-        <EuiFlexItem>
+  
+    return (
+      <div
+        style={{
+          display: "flex",
+          height: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <Header />
+        <EuiFlexGroup justifyContent="center" style={{ margin: "1rem" }}>
+          <EuiFlexItem>
             <EuiPanel>
-                <EuiBasicTable 
-                items={meetings}
-                columns={columns} />
+              <EuiBasicTable items={meetings} columns={meetingColumns} />
             </EuiPanel>
-        </EuiFlexItem>
-    </EuiFlexGroup>
-    </div>
-  );
-}
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </div>
+    );
+  }
